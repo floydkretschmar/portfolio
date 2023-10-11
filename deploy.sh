6e87e02c2b63b05f7864d0c5aac51f5d29e71e22
floydkretschmar <fkretschmar@googlemail.com>
2023-10-11 23:21:02 +0200

    Cleanup and fixing package.json

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,6 @@
 
 set -e
 
-# cd  dist
-# git config --global user.email "fkretschmar@googlemail.com"
-# git config --global user.name "floydkretschmar"
-# git init 
-# git remote set-url origin https://$GITHUB_REPOSITORY_OWNER:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
-# git checkout -b gh-pages
-# git add -A
-# git commit -m "new deployment"
-# git push
-# cd -
-
 git config --global user.email "fkretschmar@googlemail.com"
 git config --global user.name "floydkretschmar"
 git fetch origin
