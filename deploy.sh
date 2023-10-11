973c11177f0b4ec9f98e1c08677a6c880d3c60f8
floydkretschmar <fkretschmar@googlemail.com>
2023-10-11 21:19:41 +0200

    Add missing extension

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ git config --global user.name "floydkretschmar"
 git init 
 echo $GITHUB_REPOSITORY
 echo $GITHUB_REPOSITORY_OWNER
-git remote set-url origin https://$GITHUB_REPOSITORY_OWNER:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY
+git remote set-url origin https://$GITHUB_REPOSITORY_OWNER:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
 git checkout -b gh-pages
 git add -A
 git commit -m "new deployment"
