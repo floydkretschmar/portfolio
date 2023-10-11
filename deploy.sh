eb3ec97f699342cb0593f5777fa184801b377abf
floydkretschmar <fkretschmar@googlemail.com>
2023-10-11 22:24:16 +0200

    change copy

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ git config --global user.email "fkretschmar@googlemail.com"
 git config --global user.name "floydkretschmar"
 git checkout -b gh-pages
 rm -r *
-cp -r ../dist .
+cp -r ../dist/* .
 ls
 git add -A
 git diff
