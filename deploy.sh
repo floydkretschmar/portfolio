79a52cbf42f2c24baadc4ee63510740509f7ffa7
floydkretschmar <fkretschmar@googlemail.com>
2023-10-11 23:47:44 +0200

    do not remove CNAME during redeployment

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ git config --global user.email "fkretschmar@googlemail.com"
 git config --global user.name "floydkretschmar"
 git fetch origin
 git switch -c gh-pages origin/gh-pages
+mv CNAME ../dist/
 rm -r *
 cp -r ../dist/* .
 git add -A
