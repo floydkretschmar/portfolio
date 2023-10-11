f669d98489b24d9bfcc5b17411dbf9c747dcaa70
floydkretschmar <fkretschmar@googlemail.com>
2023-10-11 21:34:31 +0200

    Use origin in remote add

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ cd  dist
 git config --global user.email "fkretschmar@googlemail.com"
 git config --global user.name "floydkretschmar"
 git init 
-git remote add gh-pages https://$GITHUB_REPOSITORY_OWNER:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
+git remote add origin https://$GITHUB_REPOSITORY_OWNER:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
 git checkout -b gh-pages
 git add -A
 git commit -m "new deployment"
