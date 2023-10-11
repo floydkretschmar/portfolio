c7608a6cd75d1ecfc0be403e775cd2750b65d4f8
floydkretschmar <fkretschmar@googlemail.com>
2023-10-11 21:30:56 +0200

    Use add instead of set-url

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,7 @@ git config --global user.name "floydkretschmar"
 git init 
 echo $GITHUB_REPOSITORY
 echo $GITHUB_REPOSITORY_OWNER
-# git remote set-url origin https://$GITHUB_REPOSITORY_OWNER:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
-git remote set-url origin https://github.com/floydkretschmar/portfolio.git
-git checkout -b gh-pages
+git remote add gh-pages https://$GITHUB_REPOSITORY_OWNER:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
 git add -A
 git commit -m "new deployment"
 git push
