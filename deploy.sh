bbc98dbed9bba6fa20274ef90fbff7fa6ee8c5df
floydkretschmar <fkretschmar@googlemail.com>
2023-10-11 21:33:38 +0200

    Switch to branch before commit

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,8 @@ cd  dist
 git config --global user.email "fkretschmar@googlemail.com"
 git config --global user.name "floydkretschmar"
 git init 
-echo $GITHUB_REPOSITORY
-echo $GITHUB_REPOSITORY_OWNER
 git remote add gh-pages https://$GITHUB_REPOSITORY_OWNER:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
+git checkout -b gh-pages
 git add -A
 git commit -m "new deployment"
 git push
