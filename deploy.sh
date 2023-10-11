3a48d9b9d77e89c16822b2dc82759bb8c6a8581c
floydkretschmar <fkretschmar@googlemail.com>
2023-10-11 22:12:31 +0200

    Try existing repo

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,21 @@
 
 set -e
 
-cd  dist
+# cd  dist
+# git config --global user.email "fkretschmar@googlemail.com"
+# git config --global user.name "floydkretschmar"
+# git init 
+# git remote set-url origin https://$GITHUB_REPOSITORY_OWNER:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
+# git checkout -b gh-pages
+# git add -A
+# git commit -m "new deployment"
+# git push
+# cd -
+
 git config --global user.email "fkretschmar@googlemail.com"
 git config --global user.name "floydkretschmar"
-git init 
-git remote add origin https://$GITHUB_REPOSITORY_OWNER:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
 git checkout -b gh-pages
+rm -r *
+cp -r ../dist/* .
 git add -A
 git commit -m "new deployment"
-git push
-cd -
